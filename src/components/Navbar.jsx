9ae01ecc3ec58e11c1e1ca6d7450b46385306342
gaeng <78129949+hyeonkyeong31@users.noreply.github.com>
2023-11-30 16:48:11 +0900

    feat :  admin일 경우에만 편집 아이콘 띄우기

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,16 @@ import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
 import { login, logout, onUserStateChange } from "../api/firebase";
 import User from "./User";
+import Button from "./ui/Button";
 
 export default function Navbar() {
     const [user, setUser] = useState();
 
     useEffect(() => {
-        onUserStateChange(setUser);
+        onUserStateChange((user) => {
+            console.log(user);
+            setUser(user);
+        });
     }, []);
     return (
         <header className="flex justify-between border-b border-gray-300 p-2">
@@ -20,12 +24,16 @@ export default function Navbar() {
             <nav className="flex items-center gap-4 font-semibold">
                 <Link to="/products">Products</Link>
                 <Link to="/carts">Carts</Link>
-                <Link to="/products/new" className="text-2xl">
-                    <BsFillPencilFill />
-                </Link>
+
+                {user && user.isAdmin && (
+                    <Link to="/products/new" className="text-2xl">
+                        <BsFillPencilFill />
+                    </Link>
+                )}
+
                 {user && <User user={user} />}
-                {!user && <button onClick={login}>Login</button>}
-                {user && <button onClick={logout}>Logout</button>}
+                {!user && <Button text={"login"} onClick={login} />}
+                {user && <Button text={"logout"} onClick={logout} />}
             </nav>
         </header>
     );
