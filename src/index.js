7418338b04379eb0ed958bf98b5bfa9524fd77e7
gaeng <78129949+hyeonkyeong31@users.noreply.github.com>
2023-11-30 11:35:00 +0900

    feat : 라우터 header 기능추가, style(tailwindcss)추가

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import ProductDetail from "./pages/ProductDetail";
+import NewProduct from "./pages/NewProduct";
+import MyCart from "./pages/MyCart";
+import NotFound from "./pages/NotFound";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <NotFound />,
+        /* App이라는 부모 컴포넌트 안에 아울렛을 사용해서 children을 보여줄것임 */
+        children: [
+            { index: true, path: "/", element: <Home /> },
+            { path: "/products", element: <AllProducts /> },
+            { path: "/products/new", element: <NewProduct /> },
+            { path: "/products/:id", element: <ProductDetail /> },
+            { path: "/carts", element: <MyCart /> },
+        ],
+    },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
